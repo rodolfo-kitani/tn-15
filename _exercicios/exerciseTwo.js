20e2cd6c4465de1459a50ab44ec01b8cf0d8d299
Rodolfo Santana <rdmoedas@gmail.com>
2020-12-23 19:49:29 -0300

    add commenting in exerciseTwo.js

--- _exercicios/exerciseTwo.js
+++ _exercicios/exerciseTwo.js
@@ -13,9 +13,10 @@ foraDaSequencia([1, 2, 3, 4, 5, 6, 20, 7, 8, 9]) //retorna 20
 */
 
 function outOfSequence(array) {
+    //This will check if the next position of the array is a sequence of the current one.
     for (let i = 0; i < array.length; i++) {
       if(array[i]+1 !== array[i+1]) {
-        return array[i+1];
+        return array[i+1]; // return the number that is out of the sequence
       }
     }
     return false;
