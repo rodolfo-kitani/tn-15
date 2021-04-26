b166776671932fd65cef56932af57ab07df44ffc
Rodolfo Santana <rdmoedas@gmail.com>
2021-04-26 02:06:20 -0300

    count to 100 without numbers

--- series.js
+++ series.js
@@ -1,2 +1,26 @@
-let series = ['Gilmores Girls', 'Supernatural', 'Batman', 'X-Files']
-console.log(series[2])
+// let series = ['Gilmores Girls', 'Supernatural', 'Batman', 'X-Files']
+// console.log(series[2])
+
+
+const series = [ 
+    { "id": 1, "name": "Friends" }, 
+    { "id": 2, "name": "Breaking Bad" }, 
+    { "id": 3, "name": "Dexter" }, 
+    { "id": 4, "name": "Six Feet Under" } 
+]
+
+let idEscolhido = 3;
+
+function rota() {
+    let serieId = idEscolhido;
+    let serieEscolhida = {};
+    series.forEach(function(serie) {
+        if (serie.id === serieId) {
+            return serieEscolhida = serie;
+        }
+    })
+    console.log(serieEscolhida)
+
+}
+
+rota();
